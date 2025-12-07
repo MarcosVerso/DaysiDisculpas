480aa68b408489b0fc27b9d3d7e8873f1ef8b431
Marcos Carreño <kgmc122@gmail.com>
2025-12-07 13:43:31 -0400

    prueba 1 en index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = document.getElementById("app");
 function renderApp(){
     app.innerHTML = `
         <div class="container1">
-        <img src="https://drive.google.com/thumbnail?id=1qvcWttCiGbPp_7TZTxlKQUF3emhJ_McK" width="500px">
+        <img src="foto2.jpeg" width="500px">
             Me perdonas?
         </div>
     `;
